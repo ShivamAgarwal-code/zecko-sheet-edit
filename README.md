982111820ef76471af416bdd7ef48f0bea3ef14c
Shivam Agarwal <72163800+ShivamAgarwal-code@users.noreply.github.com>
2022-07-28 13:38:15 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,92 @@
-# zecko-sheet-edit
-Website Framework detector using Google Sheets AP
+
+# Zecko Website Category Assigner
+
+A Simple JavaScript program which finds out the framework of E-commerce websites and assigns them framework categories respectively in a separate column of a pre-existing Google Sheet, using Express and Google Sheets API in Google Cloud Platform
+
+
+## Local Setup
+
+Clone the project
+
+```bash
+git clone https://github.com/ShivamAgarwal-code/zecko-sheet-edit.git
+```
+
+Go to the root project directory
+
+```bash
+cd zecko-sheet-edit
+```
+
+
+## Installation
+
+For intializing new npm project :
+
+```bash
+npm init
+```
+Next install the following dependencies
+## Dependencies
+
+#### nodemon (Dev Dependency)
+
+```bash
+npm install -D nodemon --global
+```
+
+#### express, ejs, googleapis node-fetch@2
+
+```bash
+npm install express ejs googleapis node-fetch@2
+```
+
+
+## Editing Necessary Variables and Code Sections
+
+In **index.js** modify the following variables according to your spreadsheetID which can be found in the spreadsheet URL just before **/edit**
+
+For Example, in this case :
+https://docs.google.com/spreadsheets/d/1q95051Ie7ZeWSXEi5O_LNR1wI5jIynuXHri0eGDGlTc/edit
+
+Spreadsheet ID is **1q95051Ie7ZeWSXEi5O_LNR1wI5jIynuXHri0eGDGlTc**
+
+```javascript
+const spreadsheetID = "1q95051xxxxxxxxxxxxxxIynuXHri0eGDG1Tc";
+```
+
+Next setup Sheets API by creating a new project in Google Cloud Console, enabling the Google Sheets API and creating a new credential key under it. Place the credential key in the root folder and rename it to **credentials.json**
+## Usage
+
+The app by default listens on port 1337, but you can modify it with your own message using this line :
+
+```javascript
+app.listen(1337,(req,res) => console.log("Running Server Zecko"));
+```
+
+To run the program, type in terminal :
+```
+nodemon index.js
+```
+
+Then open your browser on the listening port (by default 1337) by going to :
+```
+http://localhost:1337
+```
+
+On successful request, you should see the Raw Data of the first column containing the websites, and if you check the original sheet, the categories would be updated as well.
+
+
+## Screenshots
+
+Setup Google Cloud Credentials :
+![Cloud](https://imgur.com/gallery/igqmGMg)
+
+Code :
+![Code](https://imgur.com/gallery/OQ2GVgd)
+
+Sheet Before Run :
+![Before](https://imgur.com/gallery/EuKDfpI)
+
+Sheet After Run :
+![After](https://imgur.com/gallery/NYex4CV)
---
+++ index.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const { google } = require("googleapis");
+
+const app = express();
+app.get("/", async(req, res) => {
+    const auth = new google.auth.GoogleAuth({
+        keyFile: "credentials.json",
+        scopes: "https://www.googleapis.com/auth/spreadsheets",
+    });
+
+    //Spreadsheet URL : https://docs.google.com/spreadsheets/d/1q95051Ie7ZeWSXEi5O_LNR1wI5jIynuXHri0eGDGlTc/edit#gid=0
+    const spreadsheetID = "1q95051Ie7ZeWSXEi5O_LNR1wI5jIynuXHri0eGDGlTc";
+
+    //Client Instance
+    const client = await auth.getClient();
+
+    //Google Sheets API Instance
+    const googleSheets = google.sheets({ version: "v4", auth: client });
+
+    //Read First Row of Websites
+    const getRows = await googleSheets.spreadsheets.values.get({
+        auth,
+        spreadsheetId: spreadsheetID,
+        range: "TestRun!A:A",
+    });
+
+    const websites = getRows.data.values;
+    let categories = [
+        ["Category"]
+    ];
+    const categoriesList = [
+        "SHOPIFY", //0
+        "WOOCOMMERCE", //1
+        "BIGCOMMERCE", //2
+        "MAGENTO", //3
+        "OTHERS", //4
+        "NOT_WORKING", //5
+    ];
+    const sz = websites.length;
+    for (let i = 1; i < sz; i++) {
+        var categoryInd = 4; //DEFAULT=OTHERS
+
+        //FETCH
+        try {
+            const fetch = require("node-fetch");
+            const result = await fetch(websites[i]);
+            const html = await result.text();
+            if (html.match("shopify") != null) {
+                categoryInd = 0;
+            } else if (html.match("woocommerce") != null) {
+                categoryInd = 1;
+            } else if (html.match("bigcommerce") != null) {
+                categoryInd = 2;
+            } else if (html.match("magento") != null) {
+                categoryInd = 3;
+            }
+        } catch (err) {
+            categoryInd = 5;
+            //Website Not Working
+            //res.send('err : ',err);
+        }
+        //FETCH ENDS
+
+        categories.push([categoriesList[categoryInd]]);
+    }
+
+    console.log(categories); //Debug frameworks to console
+
+    //Write to Second Row
+    await googleSheets.spreadsheets.values.append({
+        auth,
+        spreadsheetId: spreadsheetID,
+        range: "TestRun!B:B",
+        valueInputOption: "USER_ENTERED",
+        resource: {
+            values: categories,
+        },
+    });
+
+    res.send(getRows.data);
+});
+
+app.listen(1337, (req, res) => console.log("Running Server Zecko"));
